cdd4e3e3ddf30127c6dcef815b8e4b9ccc6052d8
venomhaxor <68974601+venomhaxor@users.noreply.github.com>
2021-01-07 15:02:47 -0600

    Update xmas.py

--- xmas.py
+++ xmas.py
@@ -51,59 +51,9 @@ from datetime import date
 
 today = date.today()
 print("Today's date:", today)
-print ("———————————")
+
 print ("Todays Time Is Below")
-print("———————————")
+
 import time
 print (time.strftime("%H:%M:%S"))
 import time ; time.sleep(2)
-import sys
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', help='target', dest='target')
-parser.add_argument('-c', help='choice', dest='choice')
-parser.add_argument('--domains', help='stdin type: domain', dest='domains', action='store_true')
-parser.add_argument('--ips', help='stdin type: ip', dest='ips', action='store_true')
-args = parser.parse_args()
-
-ips = args.ips
-target = args.target
-choice = args.choice
-domains = args.domains
-data = False
-if ips or domains:
-    data = sys.stdin.readlines()
-
-arged = False
-if target and choice:
-    arged = True
-
-if sys.version_info < (3, 0):
-    input = raw_input
-def menu():
-    print('''
-%s1.%s trace ''')
-def dog(choice, target):
-    if not args.target:
-        banner()
-    if arged:
-        hq(choice, target)
-    else:
-        while True:
-            menu()
-            result = False
-            choice = input('\033[1;91m>>\033[0m ')
-            hq(choice)
-if data:
-    kind = 'domain'
-    if ips:
-        kind = 'ip'
-    targets = extractor(data, kind)
-    if choice:
-        for target in targets:
-            print ('%s %s' % (run, target))
-            hq(choice, target)
-            print (red + ('-' * 60) + end)
-    else:
-        for target in targets:
-            sys.stdout.write(target + '\n')
